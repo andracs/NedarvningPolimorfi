149732dbed0e00ee36159af598189281e5325ed0
András Ács <acsandras@gmail.com>
2020-11-10 13:40:14 +0100

    Nu med opdateret klassediagram

---
+++ src/Persistens.java
@@ -0,0 +1,55 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 10/11/2020
+ */
+
+public class Persistens {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Persistens p = new Persistens();
+        p.save();
+        p.load();
+    }
+
+
+    public void save() throws FileNotFoundException {
+
+        java.io.File file = new java.io.File("data/varer.txt");
+        // Create a file
+        java.io.PrintWriter output = new java.io.PrintWriter(file);
+
+        // Write formatted output to the file
+        output.print("Bønner ");
+        output.println(10);
+        output.print("Æbler ");
+        output.println(85);
+
+        // Close the file
+        output.close();
+    }
+
+    public void load() throws FileNotFoundException {
+        // Create a File instance
+        java.io.File file = new java.io.File("data/varer.txt");
+
+        // Create a Scanner for the file
+        Scanner input = new Scanner(file);
+
+        // Read data from a file
+        while (input.hasNext()) {
+            String vare = input.next();
+            int antal = input.nextInt();
+            System.out.println(
+                    vare + " " + antal);
+        }
+
+        // Close the file
+        input.close();
+    }
+}
+
