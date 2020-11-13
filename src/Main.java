995560301f7fb4d597b6814e5d1384612e4eca2f
András Ács <acsandras@gmail.com>
2020-11-13 10:21:41 +0100

    Har i undervisningen refaktoreret og lavet små rettelser

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import fruits.*;
+import jdk.nashorn.api.tree.ArrayLiteralTree;
 import main.*;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.InputMismatchException;
 
 /**
  * Developed by András Ács (acsandras@gmail.com)
@@ -11,7 +16,7 @@ import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         Apple a = new Apple();
         a.moden = true;
@@ -25,6 +30,53 @@ public class Main {
 
         Konserver apple = (Konserver) new Apple();
 
+        // SCENARIO 1 --> DER ER FEJL så kører 1 og 2 og 3
+        // SCENARIO 2 --> DER ER IKKE FEJL så kører 1 og 3
+
+        /*1*/ try {
+            dummy();
+            dummy();
+            dummy();
+        /*2*/
+        } catch (SpeedTooHighException e) {
+            System.out.println("La være med at køre for hurtigt.");
+            // bil.hastighed = bil.hastighed-2;
+        }
+        /*3*/ finally {
+            // Den kører altid
+            // skal køre
+        }
+
+        /*4*/
+
+
     }
+
+    public static void dummy() throws SpeedTooHighException {
+
+        ArrayList<Mælk> mælkekarton = new ArrayList();
+        ArrayList<Grønsag> grønneting;
+        ArrayList<Vare> varelager = new ArrayList();
+        ArrayList<Konserver> konserver = new ArrayList<>();
+
+        Peas x = new Peas();
+
+        // x objektet kan tage flere former --> Polymorfisme
+        // den  kan både være Peas, Grønsag, Vare
+
+        //mælkekarton.add(x);
+        varelager.add(x);
+        konserver.add(x);
+
+        throw new SpeedTooHighException("Du må ikke køre mere end 150.");
+
+        //Encapsulation
+        //Inheritance
+        //Polymorfisme
+
+    }
+
+
+
 
 }
---
+++ src/main/SpeedTooHighException.java
@@ -0,0 +1,13 @@
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 13/11/2020
+ */
+package main;
+
+public class SpeedTooHighException extends Exception {
+
+    public SpeedTooHighException(String s) {
+    }
+}
